f43749226ecb3908d931bba6cb3bda2d7e5db8fc
pouria <arefijamal.pouria@gmail.com>
2022-02-08 18:17:41 +0330

    Front end init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from '@material-ui/core';
+import SignIn from "./Components/Signin";
+const App = () => {
+    return (
+        <div>
+            <SignIn />
+        </div>
+    );
+}
+
+export default App;
