d47da7096d625e07e2410da3543a2d212faa8d42
pouria <arefijamal.pouria@gmail.com>
2022-02-27 19:10:54 +0330

    routes edited for dashboard we have /dashboard for login /login and register not care for us

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from "react";
 import './App.css'
 import { Typography } from '@material-ui/core';
-import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Link, Redirect, Route, Switch} from "react-router-dom";
 import SignIn from "./routes/Signin";
 import HeaderComponent from "./routes/Dashboard/Header/HeaderComponent";
 import LineChart from "./Components/LineChart";
@@ -14,6 +14,7 @@ import store from "./store";
 import {Login} from "./routes/Login";
 import SendData from "./Components/SendData";
 import {loadUser} from "./Actions/auth";
+import Navbar from "./routes/Dashboard/Header/testNavbar";
 
 class App extends Component {
     componentDidMount() {
@@ -34,10 +35,13 @@ class App extends Component {
                     {/*    /!*</Route>*!/*/}
                     {/*</Routes>*/}
                     <Switch>
-                        <PrivateRoute exact path="/" component={HeaderComponent} />
+                        <PrivateRoute exact path="/admin" component={HeaderComponent} />
+                        <Route exact path="/" render={props => {
+                                return <Redirect to="/admin"/> ;
+                        }} />
                         <Route exact path="/register" component={Register} />
                         <Route exact path="/login" component={SignIn} />
-                        <Route exact path="/test" component={Login} />
+                        <PrivateRoute exact path="/test" component={Navbar} />
                     </Switch>
                 </BrowserRouter>
 
