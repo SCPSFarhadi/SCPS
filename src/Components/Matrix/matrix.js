53275314babef2e1706bd352aff18ab2dd37497f
pouria <arefijamal.pouria@gmail.com>
2022-02-27 19:20:16 +0330

    matrix for integration added. working on limiting inputs...

--- src/Components/Matrix/matrix.js
+++ src/Components/Matrix/matrix.js
@@ -12,43 +12,51 @@ function Matrix(props) {
     );
 
     function MatrixInput(props) {
-        let matrix = Array(matrixSize.rows)
-        for (let i = 0; i < matrixSize.rows; i++) {
-            matrix[i] = new Array(matrixSize.columns).fill(0)
-        }
-        const handleSubmit = event => {
-            event.preventDefault();
-            let count = 0;
+        if(matrixSize.rows>0){
+            let matrix = Array(matrixSize.rows)
             for (let i = 0; i < matrixSize.rows; i++) {
-                for (let j = 0; j < matrixSize.columns; j++) {
-                    // If the floating point number cannot be parsed, we set 0 for this value
-                    matrix[i][j] = !isNaN(parseFloat(event.target[count].value)) ? parseFloat(event.target[count].value) : 0;
-                    count += 1;
-                }
+                matrix[i] = new Array(matrixSize.columns).fill(0)
             }
-            setMatrix(matrix);
+            const handleSubmit = event => {
+                event.preventDefault();
+                let count = 0;
+                for (let i = 0; i < matrixSize.rows; i++) {
+                    for (let j = 0; j < matrixSize.columns; j++) {
+                        // If the floating point number cannot be parsed, we set 0 for this value
+                        matrix[i][j] = !isNaN(parseFloat(event.target[count].value)) ? parseFloat(event.target[count].value) : 0;
+                        count += 1;
+                    }
+                }
+                setMatrix(matrix);
+            }
+            return (
+                <form onSubmit={handleSubmit}>
+                    {matrix.map((row, indexRow = 1) => {
+                        return (
+                            <div style={{display: 'flex'}} key={indexRow}>
+                                {row.map((item, indexColumn = 1) => {
+                                    return (
+                                        <input
+                                            key={indexRow + " " + indexColumn}
+                                            type="text"
+                                            defaultValue={0}
+                                            name={indexRow + "," + indexColumn}
+                                        />
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+                    <button>{"Save A"}</button>
+                </form>
+            );
         }
-        return (
-            <form onSubmit={handleSubmit}>
-                {matrix.map((row, indexRow = 1) => {
-                    return (
-                        <div style={{display: 'flex'}} key={indexRow}>
-                            {row.map((item, indexColumn = 1) => {
-                                return (
-                                    <input
-                                        key={indexRow + " " + indexColumn}
-                                        type="text"
-                                        defaultValue={0}
-                                        name={indexRow + "," + indexColumn}
-                                    />
-                                )
-                            })}
-                        </div>
-                    )
-                })}
-                <button>{"Save A"}</button>
-            </form>
-        );
+        else{
+            return(
+                <h3>please input a valid number!</h3>
+            )
+        }
+
     }
 
     return (
