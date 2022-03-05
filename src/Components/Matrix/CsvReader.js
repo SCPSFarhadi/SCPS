7d30188f06be3af2ad1991052bc1113003bb9493
pouria <arefijamal.pouria@gmail.com>
2022-03-05 18:03:39 +0330

    Parser Csv added

--- src/Components/Matrix/CsvReader.js
+++ src/Components/Matrix/CsvReader.js
@@ -2,14 +2,15 @@
 import React, {useState} from 'react';
 import ReactFileReader from 'react-file-reader';
 import Button from "@mui/material/Button";
-import {async} from "async";
+import Papa  from 'papaparse';
 
 
 function CsvReader(props) {
     const handleFiles = (files) => {
         Array.from(files).forEach(async (file) =>{
             const text = await file.text();
-            console.log(text)
+            const result = Papa.parse(text)
+            console.log(result)
         })
     }
 
