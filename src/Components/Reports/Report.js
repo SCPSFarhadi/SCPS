cb6c3d91a4f718df464ce3551dd7e52a41e5b6c8
pouria <arefijamal.pouria@gmail.com>
2022-09-09 17:24:58 +0430

    all chnges added

--- src/Components/Reports/Report.js
+++ src/Components/Reports/Report.js
@@ -190,8 +190,8 @@ export default function ReportStates() {
             <GridColumn field="Temp"  title="Temperature"/>
             <GridColumn field="Humidity" title="Humidity Sensor"/>
             <GridColumn field="Light" title="Light Sensor"/>
-            <GridColumn field="AnalogSensor1" title="Analog Sensor 1"/>
-            <GridColumn field="AnalogSensor2" title="Analog Sensor 2"/>
+            <GridColumn field="AnalogSensor1" title="Solid humidity sensor 1"/>
+            <GridColumn field="AnalogSensor2" title="Solid humidity sensor 2"/>
         </Grid>
             ;
 
