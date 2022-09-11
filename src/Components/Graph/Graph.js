5556c2aa093b8b17dd108a93ddda608d53df0b8a
pouria <arefijamal.pouria@gmail.com>
2022-09-11 18:25:19 +0430

    colors added

--- src/Components/Graph/Graph.js
+++ src/Components/Graph/Graph.js
@@ -193,7 +193,12 @@ function MakeGraph(props) {
     const [selectedNode, setSelectedNode] = React.useState("");
     const [nodeColor, setColor] = React.useState(green);
     const [dataState, setData] = React.useState(props.data);
+    let backImage = useSelector(()=>props.backGroundImage)
 
+    let IMageUrl = `url(https://s6.uupload.ir/files/screenshot_from_2022-09-11_11-24-51_lajk.png)`
+    if(backImage){
+        IMageUrl = `url(${props.backGroundImage})`
+    }
     const handleClickOpen = (node) => {
         setSelectedNode(node);
         setOpen(true);
@@ -340,7 +345,7 @@ function MakeGraph(props) {
                                     <React.Fragment>
                                         <div>
                                             {/*backgroundImage:`url(${props.backGroundImage})`*/}
-                                            <div id='graphDiv' style={{height:'600px',width:'100%',backgroundImage:`url(http://37.156.25.234:8000/media/users/None/2022090319521763.png)`,backgroundRepeat:"no-repeat",backgroundSize: "contain",backgroundPosition:"center"}}>
+                                            <div id='graphDiv' style={{height:'600px',width:'100%',backgroundImage: IMageUrl,backgroundRepeat:"no-repeat",backgroundSize: "contain",backgroundPosition:"center"}}>
                                                 {/*<Graph*/}
                                                 {/*    id="graph-id" // id is mandatory*/}
                                                 {/*    data={props.data}*/}
