e68c7f999a217b3aa33a4b0c8d8848820d274dcf
pouria <arefijamal.pouria@gmail.com>
2022-09-13 11:20:12 +0430

    colors added

--- src/routes/Dashboard/Header/Navbar.jsx
+++ src/routes/Dashboard/Header/Navbar.jsx
@@ -516,7 +516,7 @@ function DashboardContent(props) {
                                     noWrap
                                     sx={{ flexGrow: 2 }}
                                 >
-                                    Farm Max Temperature: {tempMax} / Room ID :{IdMax}
+                                    Farm Max Temperature: {tempMax} / Node ID :{IdMax}
                                 </Typography>
 
                                 <Typography
@@ -526,7 +526,7 @@ function DashboardContent(props) {
                                     noWrap
                                     sx={{ flexGrow: 2 }}
                                 >
-                                    Farm Min Temperature: {tempMin} / Room ID :{IdMin}
+                                    Farm Min Temperature: {tempMin} / Node ID :{IdMin}
                                 </Typography>
                                 {/*<Typography*/}
                                 {/*    component="h4"*/}
